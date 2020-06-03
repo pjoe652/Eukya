0201d22bfe78d04000b8886ce1878dad83995e69
Peter Joe <Peter.Joe@pgtrust.co.nz>
2020-06-03 14:09:48 +1200

    Fixed catalogue and router

--- eukya/pages/index.js
+++ eukya/pages/index.js
@@ -1,48 +1,78 @@
 import Head from 'next/head'
+import Router from 'next/router'
 
-export default function Home() {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Eukya Furniture</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'/>
-        <script src="https://kit.fontawesome.com/ca7ff47d1e.js" crossOrigin="anonymous"/>
-      </Head>
-      <header>
-        <div className="title-container">
-          <a className="title">
-            EUKYA
-          </a>
-          <a className="subcase">
-            furniture
-          </a>
-        </div>
-        <div className="catalogue-container">
-          <div className="category">
-            <a>Restaurant</a>
-            <div className="zoom-container">
-              <div className="mask"/>
-              <img src="/restaurant.jpg"/>
-            </div>
-          </div>
-          <div className="category">
-            <a>Hotel</a>
-            <div className="zoom-container">
-              <div className="mask"/>
-              <img src="/hotel.jpg"/>
-            </div>
-          </div>
-          <div className="category">
-            <a>Banquet</a>
-            <div className="zoom-container">
-              <div className="mask"/>
-              <img src="/banquet.jpg"/>
-            </div>
-          </div>
-        </div>
-      </header>
+class Home extends React.Component {
+  state = {
+    hoveredItem: null
+  }
 
-    </React.Fragment>
-  )
+  onCategoryHover = category => {
+    this.setState({
+      hoveredItem: category
+    })
+  }
+
+  onCategoryClick = category => {
+    Router.push(`/${category}`)
+  }
+
+  render() {
+    const { hoveredItem } = this.state
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Eukya Furniture</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'/>
+          <script src="https://kit.fontawesome.com/ca7ff47d1e.js" crossOrigin="anonymous"/>
+        </Head>
+        <header>
+          <div className="title-container">
+            <a className="title">
+              EUKYA
+            </a>
+            <a className="subcase">
+              furniture
+            </a>
+          </div>
+          <div className="catalogue-container">
+            <div className="category" 
+              onClick={() => this.onCategoryClick('restaurant')}
+              onMouseEnter={() => this.onCategoryHover('restaurant')}
+              onMouseLeave={() => this.onCategoryHover(null)}
+              >
+              <a>Restaurant</a>
+              <div className="zoom-container">
+                <img className={`${hoveredItem === 'restaurant' ? 'selected' : ''}`} src="/restaurant.jpg"/>
+              </div>
+            </div>
+            <div className="category" 
+              onClick={() => this.onCategoryClick('hotel')}
+              onMouseEnter={() => this.onCategoryHover('hotel')}
+              onMouseLeave={() => this.onCategoryHover(null)}
+            >
+              <a>Hotel</a>
+              <div className="zoom-container">
+                <img className={`${hoveredItem === 'hotel' ? 'selected' : ''}`} src="/hotel.jpg"/>
+              </div>
+            </div>
+            <div className="category" 
+              onClick={() => this.onCategoryClick('banquet')}
+              onMouseEnter={() => this.onCategoryHover('banquet')}
+              onMouseLeave={() => this.onCategoryHover(null)}
+            >
+              <a>Banquet</a>
+              <div className="zoom-container">
+                <img className={`${hoveredItem === 'banquet' ? 'selected' : ''}`} src="/banquet.jpg"/>
+              </div>
+            </div>
+          </div>
+        </header>
+  
+      </React.Fragment>
+    )
+  }
 }
+
+export default Home

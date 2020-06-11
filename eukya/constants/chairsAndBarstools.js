71c1e652aee97b313398703cee41b77fc9bb1c3a
Peter Joe <Peter.Joe@pgtrust.co.nz>
2020-06-11 16:10:03 +1200

    Responsiveness added and map loop for items

--- eukya/constants/chairsAndBarstools.js
+++ eukya/constants/chairsAndBarstools.js
@@ -1,12 +1,27 @@
-const chairsAndBarstools = [
+export const chairsAndBarstools = [
   {
     name: "Item 1",
     description: "This is Item 1",
-    src: "/item1.png"
+    src: "/restaurant.jpg"
   },
   {
     name: "Item 2",
     description: "This is Item 2",
-    src: "/item2.png"
+    src: "/hotel.jpg"
+  },
+  {
+    name: "Item 3",
+    description: "This is Item 3",
+    src: "/banquet.jpg"
+  },
+  {
+    name: "Item 4",
+    description: "This is Item 4",
+    src: "/banquet.jpg"
+  },
+  {
+    name: "Item 5",
+    description: "This is Item 5",
+    src: "/banquet.jpg"
   }
 ]
--- eukya/pages/restaurant.js
+++ eukya/pages/restaurant.js
@@ -1,8 +1,67 @@
 import Header from "./header"
+import { chairsAndBarstools } from '../constants/chairsAndBarstools'
+import { Desktop, Phone, DesktopLg, Tablet, TabletLand } from '../constants/screenWidth'
 
 class Restaurant extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidUpdate() {
+    window.addEventListener('resize', this.updateShowcaseRow)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateShowcaseRow)
+  }
+
+  componentDidMount() {
+    this.updateShowcaseRow()
+  }
+
+  updateShowcaseRow = () => {
+    let itemsPerRow = 4
+    let tempColumnItems = []
+    let tempArray = []
+
+    console.log(window.innerWidth)
+
+    if (window.innerWidth < Tablet) {
+      itemsPerRow = 1
+    } else if (window.innerWidth < TabletLand) {
+      itemsPerRow = 2
+    } else if (window.innerWidth < DesktopLg) {
+      itemsPerRow = 3
+    }
+
+    chairsAndBarstools.forEach((item, index) => {
+      let row = Math.floor(index/itemsPerRow)
+      item.empty = false
+      tempArray.push(item)
+      if (tempArray.length === itemsPerRow) {
+        tempColumnItems.push(tempArray)
+        tempArray = []
+      }
+    })
+
+    while (tempArray.length < itemsPerRow) {
+      tempArray.push({empty: true})
+    }
+
+    tempColumnItems.push(tempArray)
+
+    this.setState({
+      items: tempColumnItems
+    })
+  }
 
   render() {
+    const { items } = this.state
+    console.log(items)
+
     return(
       <React.Fragment>
         <div className="main-container">
@@ -23,19 +82,26 @@ class Restaurant extends React.Component {
               <div className="title">
                 Chairs and Barstools
               </div>
-              <div className="item">
-                <img src="/jackinthebox.png"/>
-              </div>
-              <div className="item">
-                <img src="/jackinthebox.png"/>
-              </div>
-              <div className="item">
-                <img src="/jackinthebox.png"/>
-              </div>
-              <div className="item">
-                <img src="/jackinthebox.png"/>
-              </div>
-              
+              {
+                items.map((row, cIndex) => {
+                  return(
+                    <div key={`row-${cIndex}`} className="item-image-row">
+                      {
+                        row.map((item, index) => {
+                          console.log(item)
+                          return(
+                            <div key={`row-${cIndex} item-${index}`} className="item-image-container">
+                              {
+                                !item.empty && <img src={item.src}/>
+                              }
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div>

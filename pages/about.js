53aa2fc3455b68e37e492b85aea7ff3a8c71e269
zhangrj <zhangrj@rose-hulman.edu>
2020-06-19 02:08:22 -0700

    stuff

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Header from "../components/header"
+import Footer from '../components/footer'
 
 class About extends React.Component {
 
@@ -8,27 +9,27 @@ class About extends React.Component {
           <Header/>
           <div className="about-container">
             <div className="title">
-                <a>About Us</a>
+              <a>About Us</a>
             </div>
-            <div className="scnd-container">
-              <div className="third-container">
-                <div className="fifth-container">
-                  <a> Jimmy Zhang</a>
-                </div>
+            <div className="coverimage">
+              <img src="warehouse.png"></img>
+            </div>
+            <div className="whoweare">
+              <div className="whonow">
+                <a className="question"> Who We Are </a>
+                <a>Our company was formed in 2001 in Los Angeles, California with the goal of becoming an integral supplier of contract furnishings to the foodservice and hospitality industries. In total our management team now boasts decades of specific industry experience that shows throughout our daily operation. The growth we have experienced is a testament to our dedication to quality, competitiveness, on-time deliveries, and un-matched customer service.</a>
               </div>
-              <div className="fourth-container">
-                <a>
-                    Our company was formed in 2001 in Los Angeles, California with the goal of becoming an integral supplier of contract furnishings to the foodservice and hospitality industries. In total our management team now boasts decades of specific industry experience that shows throughout our daily operation. The growth we have experienced is a testament to our dedication to quality, competitiveness, on-time deliveries, and un-matched customer service.
-                </a>
-                <a>
-                    We are manufacturers and importers of contract furniture committed to delivering consistent product to the marketplace. We ship from 2 convenient and strategic warehouses located in California and Illinois. In our manufacturing process we use 100% European Beechwood exclusively for all of our wood chairs and barstools as well as utilizing the finest stains, finishes, and upholsteries. Additionally, we secure the best quality steel and aluminum for use in our indoor metal product line and outdoor furniture utilizing the finest powdercoat finishes available.
-                </a>
-                <a>
-                    We take pride in servicing your needs; from the local business to the largest of chains you are assured the same attention to detail and commitment to your satisfaction. Our diverse manufacturing capabilities along with our stock products has enable us to service many markets from restaurants to hotels from schools to B & I and beyond. Whether you find something from our quick ship product offering or you are looking for something more design oriented we are eager to satisfy your furniture requirements. If you have something in mind that we don’t show on-line please let us know what you are looking for and we will see if we can manufacture it for you.
-                </a>
+              <div className="whonow">
+                <a className="question"> What We Make </a>
+                <a>We take pride in servicing your needs; from the local business to the largest of chains you are assured the same attention to detail and commitment to your satisfaction. Our diverse manufacturing capabilities along with our stock products has enable us to service many markets from restaurants to hotels. Whether you find something from our quick ship product offering or you are looking for something more design oriented we are eager to satisfy your furniture requirements. If you have something in mind that we don’t show on-line please let us know what you are looking for and we will see if we can manufacture it for you.</a>
+              </div>
+              <div className="whonow">
+                <a className="question"> How We Deliver </a>
+                <a>We are manufacturers and importers of contract furniture committed to delivering consistent product to the marketplace. We ship from a convenient and strategic warehouses located in California. In our manufacturing process we use 100% European Beechwood exclusively for all of our wood chairs and barstools as well as utilizing the finest stains, finishes, and upholsteries. Additionally, we secure the best quality steel and aluminum for use in our indoor metal product line and outdoor furniture utilizing the finest powdercoat finishes available. </a>
               </div>
             </div>
           </div>
+          <Footer/>
         </div>
     )
   }
---
+++ pages/contact.js
@@ -0,0 +1,56 @@
+import Header from "../components/header"
+import Footer from '../components/footer'
+
+class Contact extends React.Component {
+
+  render() {
+    return(
+        <div className="main-container">
+          <Header/>
+          <div className="contact-container">
+            <div className="title">
+              <a>Contact Us</a>
+            </div>
+            <div className="coverimage">
+              <img src="warehouse.png"></img>
+            </div>
+            <div className="description">
+              <a> We'd love to help! Send us a message or visit us for any help or information.</a>
+            </div>
+            <div className="twothings">
+              <div className="contactinfo">
+                  <a> Your Name </a>
+                  <input type="text"/>
+                  <a> Email </a>
+                  <input type="text"/>
+                  <a> Message </a>
+                  <input className="biginput" type="text"/>
+                  <a></a>
+                  <input type="button" className="button" value="Submit"/>
+              </div>
+              <div className="map">
+                <div className="icons">
+                  <i class="fas fa-map-marker-alt"></i>
+                </div>
+                <div className="info">
+                  <div className="onething">
+                    <a>233 S 7th Ave</a>
+                    <a>City Of Industry, CA 91746</a>
+                  </div>
+                  <div className="onething">
+                    <a> 626.354.9121</a> 
+                  </div>
+                  <div className="onething">
+                    <a> wdzhang2000@gmail.com</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer/>
+        </div>
+    )
+  }
+}
+
+export default Contact
